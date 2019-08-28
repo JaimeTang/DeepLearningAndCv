e6d950b3177c8169fb114656aff90ed094959e41
唐进民（Guilin Tang） <tjmts@qq.com>
2019-08-28 22:15:34 +0800

    Update new content

--- Project/projectII_face_keypoints_detection/data.py
+++ Project/projectII_face_keypoints_detection/data.py
@@ -22,7 +22,11 @@ def channel_norm(img):
 def parse_line(line):
     line_parts = line.strip().split()
     img_name = line_parts[0]
+    print("-"*10)
+    print(img_name)
     rect = list(map(int, list(map(float, line_parts[1:5]))))
+    print("-" * 10)
+    print(rect)
     landmarks = list(map(float, line_parts[5: len(line_parts)]))
     return img_name, rect, landmarks
 
@@ -84,8 +88,8 @@ class FaceLandmarksDataset(Dataset):
 		# you should let your landmarks fit to the train_boarder(112)
 		# please complete your code under this blank
 		# your code:
-		
-		
+
+        landmarks
 		
 		
         sample = {'image': img_crop, 'landmarks': landmarks}
@@ -97,6 +101,7 @@ def load_data(phase):
     data_file = phase + '.txt'
     with open(data_file) as f:
         lines = f.readlines()
+
     if phase == 'Train' or phase == 'train':
         tsfm = transforms.Compose([
             Normalize(),                # do channel normalization
@@ -117,27 +122,20 @@ def get_train_test_set():
     return train_set, valid_set
 
 
-if __name__ == '__main__':
-    train_set = load_data('train')
+def main():
+    ##train_set = load_data('train')
+    train_set = load_data('./data/I/label')
     for i in range(1, len(train_set)):
         sample = train_set[i]
         img = sample['image']
         landmarks = sample['landmarks']
-		## 请画出人脸crop以及对应的landmarks
-		# please complete your code under this blank
-		
-		
-		
-		
-		
-
+        ## 请画出人脸crop以及对应的landmarks
+        # please complete your code under this blank
+        cv2.imshow("img", img)
+        # plt.scatter(landmarks)
         key = cv2.waitKey()
         if key == 27:
-            exit(0)
-        cv2.destroyAllWindows()
+            cv2.destroyAllWindows()
 
-
-
-
-
-
+if __name__ == '__main__':
+    train_set = load_data('./data/I/label')
