c9326748c0dd3669a58a89adf9b22ece9d7eef2e
唐进民（Guilin Tang） <tjmts@qq.com>
2019-09-08 18:31:18 +0800

    Update new content

--- Lesson-10/NMS.py
+++ Lesson-10/NMS.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def NMS(lists, thre):
+def NMS(lists, thresh):
     # lists is a list. lists[0:4]: x1, y1, x2, y2; lists[4]: score
     dets = np.array(lists)
     x1 = dets[:, 0]
@@ -23,6 +23,17 @@ def NMS(lists, thre):
         yy1 = np.maximum(y1[i], x1[order[1:]])
         xx2 = np.maximum(x1[i], x1[order[1:]])
         yy2 = np.maximum(x1[i], x1[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w*h
+
+        ovr = inter/(area[i]+area[order[1:]] - inter)
+
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
+
+        return keep
 
 
 def main():
