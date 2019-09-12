5216485f25e46eafd47a5f56425557cb0509426e
唐进民（Guilin Tang） <tjmts@qq.com>
2019-09-13 00:24:20 +0800

    Update new content

--- Lesson-11/object_detect.py
+++ Lesson-11/object_detect.py
@@ -1,10 +1,44 @@
 import torch
 import torchvision
-import PIL
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
+
+class PennFudanDataset(object):
+
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        self.imgs = list(sorted(os.listdir(os.path.join(root, 'PNGImages'))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, 'PedMasks'))))
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root, 'PNGImages', self.imgs[idx])
+        mask_path = os.path.join(self.root, 'PedMasks', self.masks[idx])
+
+        img = Image.open(img_path).convert('RGB')
+        mask = Image.open(mask_path)
+
+
+    def __len__(self):
+        return len(self.imgs)
 
 def main():
-    pass
+    root = 'PennFudanPed'
+    masks = sorted(os.listdir(os.path.join(root, 'PedMasks')))
+    mask_path = os.path.join(root, 'PedMasks', masks[1])
+    mask = Image.open(mask_path)
+    plt.imshow(mask)
+    plt.show()
+
+    mask = np.array(mask)
+    obj_ids = np.unique(mask)
+    obj_ids = obj_ids[1:]
+
+    masks = mask == obj_ids[:, None, None]
+    print(masks)
 
 
 if __name__=='__main__':
